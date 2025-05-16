362036e355bf972058d038bad4e46dc64bce52a0
Tazman <klementizarkua5@gmail.com>
2025-05-16 11:39:51 +0400

    create login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SvgIconComponent } from "../../common-ui/svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+isPasswordVisible = signal(false);
 }
