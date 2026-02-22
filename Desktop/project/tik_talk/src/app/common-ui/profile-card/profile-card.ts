38a9a39751a1248761545d1ae5431ddae223c731
Tazman <klementizarkua5@gmail.com>
2026-02-22 16:11:35 +0400

    create pipe for img

--- Desktop/project/tik_talk/src/app/common-ui/profile-card/profile-card.ts
+++ Desktop/project/tik_talk/src/app/common-ui/profile-card/profile-card.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { IProfile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url-pipe';
 
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
